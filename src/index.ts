adfd6c0a4e37e59c2c316dfca1e994459d0ad0fe
HerbCaudill <herb@devresults.com>
2020-10-21 12:09:45 +0200

    index.ts: export utilities

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 ﻿export * from '/asymmetric'
 export * from '/hash'
+export * from '/randomKey'
 export * from '/signatures'
+export * from '/stretch'
 export * from '/symmetric'
+
+export * from '/util'
