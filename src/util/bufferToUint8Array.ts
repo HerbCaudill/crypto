cb0888d5cdb6fa737b56ff9fb883457039a26efa
HerbCaudill <herb@devresults.com>
2021-03-07 17:59:31 +0100

    util/bufferToUint8Array.ts: create

---
+++ src/util/bufferToUint8Array.ts
@@ -0,0 +1,3 @@
+﻿// https://stackoverflow.com/a/31394257/239663
+export const bufferToUint8Array = (b: Buffer): Uint8Array =>
+  new Uint8Array(b.buffer, b.byteOffset, b.byteLength / Uint8Array.BYTES_PER_ELEMENT)
