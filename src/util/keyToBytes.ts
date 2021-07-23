3bd558b68dc29334020efe1d43c62b721a302c90
HerbCaudill <herb@devresults.com>
2021-07-23 14:59:26 +0200

    util/keyToBytes.ts:

--- src/util/keyToBytes.ts
+++ src/util/keyToBytes.ts
@@ -2,7 +2,7 @@
 import { base58 } from './base58'
 import { Key } from './types'
 
-export const keyToBytes = (x: Key, encoding: Encoding = 'base58') => {
+export const keyToBytes = (x: Key, encoding: Encoding = 'base58'): Uint8Array => {
   const decode = encoding === 'base58' ? base58.decode : utf8.encode
   return typeof x === 'string' ? decode(x) : x
 }
