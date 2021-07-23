71cc458e939d23ff8de649fd676ebdf71e26146e
HerbCaudill <herb@devresults.com>
2021-07-23 14:59:13 +0200

    util/base58.ts: convert buffer to array

--- src/util/base58.ts
+++ src/util/base58.ts
@@ -1,4 +1,17 @@
 ﻿import basex from 'base-x'
 
 const BASE58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-export const base58 = basex(BASE58)
+const _base58 = basex(BASE58)
+
+export const base58 = {
+  ..._base58,
+  decode: (s: string) => {
+    const b = _base58.decode(s)
+    return bufferToArray(b)
+  },
+}
+
+// https://stackoverflow.com/a/31394257/239663
+function bufferToArray(b: Buffer) {
+  return new Uint8Array(b.buffer, b.byteOffset, b.byteLength / Uint8Array.BYTES_PER_ELEMENT)
+}
