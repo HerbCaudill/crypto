7b7e419cd040a6fce25c4a0543b031f59615a537
HerbCaudill <herb@devresults.com>
2021-11-22 17:25:02 +0100

    test/hash.test.ts:

--- src/test/hash.test.ts
+++ src/test/hash.test.ts
@@ -1,23 +1,23 @@
 import { keyToBytes } from '../util'
-import * as crypto from '..'
+import { hash } from '../index'
 
 describe('crypto', () => {
   describe('hash', () => {
     const payload = 'one if by day, two if by night'
 
     it('has the correct length', () => {
-      const hash = crypto.hash(payload, 'TEST_HASH_PURPOSE')
-      expect(keyToBytes(hash).length).toBe(32)
+      const h = hash(payload, 'TEST_HASH_PURPOSE')
+      expect(keyToBytes(h).length).toBe(32)
     })
 
     it('results are deterministic', () => {
-      const hash = crypto.hash(payload, 'TEST_HASH_PURPOSE')
-      expect(hash).toMatchInlineSnapshot(`"CkG7uYzjkuKco3BW8tbAK9RwynWvsNYNDstmLj2Ark3T"`)
+      const h = hash(payload, 'TEST_HASH_PURPOSE')
+      expect(h).toMatchInlineSnapshot(`"CkG7uYzjkuKco3BW8tbAK9RwynWvsNYNDstmLj2Ark3T"`)
     })
 
     it('gives different results with different seeds', () => {
-      const hash1 = crypto.hash(payload, 'SOMETHING')
-      const hash2 = crypto.hash(payload, 'SOMETHING_ELSE')
+      const hash1 = hash(payload, 'SOMETHING')
+      const hash2 = hash(payload, 'SOMETHING_ELSE')
       expect(hash2).not.toEqual(hash1)
     })
   })
