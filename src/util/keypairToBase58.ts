901010a4524a4d0e5f43910ac917e816546ae085
HerbCaudill <herb@devresults.com>
2021-11-23 13:03:43 +0100

    util/keypairToBase58.ts:

--- src/util/keypairToBase58.ts
+++ src/util/keypairToBase58.ts
@@ -1,7 +1,7 @@
-﻿import sodium from 'libsodium-wrappers'
+﻿import { KeyPair } from 'libsodium-wrappers'
 import { base58 } from './base58'
 
-export const keypairToBase58 = (keypair: sodium.KeyPair) => ({
+export const keypairToBase58 = (keypair: KeyPair) => ({
   publicKey: base58.encode(keypair.publicKey),
   secretKey: base58.encode(keypair.privateKey),
 })
